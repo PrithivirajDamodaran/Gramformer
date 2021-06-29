69a1528e46603c3dafee27a19526f9094559d53b
Cleve Green <37374454+CleveGreen@users.noreply.github.com>
2021-06-29 17:00:52 -0400

    added change highlighter/tagger
    Marks up text with deletions and additions: <a> for addition, <d> for deletion.

--- gramformer/gramformer.py
+++ gramformer/gramformer.py
@@ -1,56 +1,92 @@
 class Gramformer:
 
-  def __init__(self, models=2, use_gpu=False):
-    from transformers import AutoTokenizer
-    from transformers import AutoModelForSeq2SeqLM
+    def __init__(self, models=2, use_gpu=False):
+        from transformers import AutoTokenizer
+        from transformers import AutoModelForSeq2SeqLM
 
-    if use_gpu:
-        device= "cuda:0"
-    else:
-        device = "cpu"
+        if use_gpu:
+            device = "cuda:0"
+        else:
+            device = "cpu"
 
-    self.device    = device
-    correction_model_tag = "prithivida/grammar_error_correcter_v1"
-    
-    if models == 2:
-        self.correction_tokenizer = AutoTokenizer.from_pretrained(correction_model_tag)
-        self.correction_model     = AutoModelForSeq2SeqLM.from_pretrained(correction_model_tag)
-        self.correction_model     = self.correction_model.to(device)
-        print("[Gramformer] Grammar error correction model loaded..")
-    elif models == 1:
-        print("[Gramformer] Highlight model not supported, yet")
-    elif models == 3:
-        self.correction_tokenizer = AutoTokenizer.from_pretrained(correction_model_tag)
-        self.correction_model     = AutoModelForSeq2SeqLM.from_pretrained(correction_model_tag)
-        self.correction_model     = self.correction_model.to(device)
-        print("[Gramformer] All models loaded..")    
-    
+        self.device = device
+        correction_model_tag = "prithivida/grammar_error_correcter_v1"
 
-  def correct(self, input_sentence, max_corrections=1):
-      correction_prefix = "gec: "
-      input_sentence = correction_prefix + input_sentence
-      input_ids = self.correction_tokenizer.encode(input_sentence, return_tensors='pt')
-      input_ids = input_ids.to(self.device)
+        if models == 2:
+            self.correction_tokenizer = AutoTokenizer.from_pretrained(correction_model_tag)
+            self.correction_model = AutoModelForSeq2SeqLM.from_pretrained(correction_model_tag)
+            self.correction_model = self.correction_model.to(device)
+            print("[Gramformer] Grammar error correction model loaded..")
+        elif models == 1:
+            print("[Gramformer] Highlight model not supported, yet")
+        elif models == 3:
+            self.correction_tokenizer = AutoTokenizer.from_pretrained(correction_model_tag)
+            self.correction_model = AutoModelForSeq2SeqLM.from_pretrained(correction_model_tag)
+            self.correction_model = self.correction_model.to(device)
+            print("[Gramformer] All models loaded..")
 
-      preds = self.correction_model.generate(
-          input_ids,
-          do_sample=True, 
-          max_length=128, 
-          top_k=50, 
-          top_p=0.95, 
-          early_stopping=True,
-          num_return_sequences=max_corrections)
+    def correct(self, input_sentence, max_corrections=1):
+        correction_prefix = "gec: "
+        input_sentence = correction_prefix + input_sentence
+        input_ids = self.correction_tokenizer.encode(input_sentence, return_tensors='pt')
+        input_ids = input_ids.to(self.device)
 
-      corrected = []
-      for pred in preds:  
-        corrected.append(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
+        preds = self.correction_model.generate(
+            input_ids,
+            do_sample=True,
+            max_length=128,
+            top_k=50,
+            top_p=0.95,
+            early_stopping=True,
+            num_return_sequences=max_corrections)
 
-      return corrected
+        corrected = []
+        for pred in preds:
+            corrected.append(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
 
-  def highlight(self, input_sentence):
-      # TO BE IMPLEMENTED
-      pass
+        return corrected
 
-  def detect(self, input_sentence):
-      # TO BE IMPLEMENTED
-      pass
+    def highlight(self, input_sentence: str, max_corrections: int = 1) -> str:
+        from difflib import ndiff
+        
+        highlighted = []
+        changes = list(ndiff(
+            input_sentence,
+            self.correct(input_sentence, max_corrections=max_corrections)[0]
+        ).__iter__())
+        
+        for i, p_e in enumerate(changes):
+            n_i, b_i = [(i + m) % len(changes) for m in [1, -1]]
+            b_i = 0 if b_i > i else b_i
+            n_i = -1 if n_i < i else n_i
+            mode = 'a' if p_e[0] == "+" else 'd'
+            
+            if p_e[0] != " ":
+                if n_i == -1:
+                    if changes[b_i][0] == p_e[0]:
+                        highlighted.append(f"{p_e[2]}</{mode}>")
+                    else:
+                        highlighted.append(f"<{mode}>{p_e[2]}</{mode}>")
+                elif i != 0 and changes[b_i][0] == p_e[0]:
+                    if changes[n_i][0] == p_e[0]:
+                        highlighted.append(f"{p_e[2]}")
+                    else:
+                        highlighted.append(f"{p_e[2]}</{mode}>")
+                elif i != 0:
+                    if changes[n_i][0] == p_e[0]:
+                        highlighted.append(f"<{mode}>{p_e[2]}")
+                    else:
+                        highlighted.append(f"<{mode}>{p_e[2]}</{mode}>")
+                elif i == 0:
+                    if changes[n_i][0] == p_e[0]:
+                        highlighted.append(f"<{mode}>{p_e[2]}")
+                    else:
+                        highlighted.append(f"<{mode}>{p_e[2]}</{mode}>")
+            else:
+                highlighted.append(p_e[2])
+                
+        return "".join(highlighted)
+
+    def detect(self, input_sentence):
+        # TO BE IMPLEMENTED
+        pass
