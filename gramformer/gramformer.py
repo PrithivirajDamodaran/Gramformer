4b288a3f172649e586b8c6cd9648fda76b4c7618
Prithivida <d.prithiviraj@gmail.com>
2021-07-04 11:18:35 +0530

    changes for QE estimator

--- gramformer/gramformer.py
+++ gramformer/gramformer.py
@@ -56,57 +56,56 @@ class Gramformer:
      # TO BE IMPLEMENTED
      pass
 
-def _get_edits(self, orig, cor):
-    orig = self.annotator.parse(orig)
-    cor = self.annotator.parse(cor)
-    alignment = self.annotator.align(orig, cor)
-    edits = self.annotator.merge(alignment)
-
-    if len(edits) == 0:  
-        return []
-
-    edit_annotations = []
-    for e in edits:
-        e = self.annotator.classify(e)
-        edit_annotations.append((e.type[2:], e.o_str, e.o_start, e.o_end,  e.c_str, e.c_start, e.c_end))
-            
-    if len(edit_annotations) > 0:
-        return edit_annotations
-    else:    
-        return []
-
-
-def get_edits(self, input_sentence):
-      correction_prefix = "gec: "
-      input_sentence = correction_prefix + input_sentence
-      input_ids = self.correction_tokenizer.encode(input_sentence, return_tensors='pt')
-      input_ids = input_ids.to(self.device)
-
-      preds = self.correction_model.generate(
-          input_ids,
-          do_sample=True, 
-          max_length=128, 
-          top_k=50, 
-          top_p=0.95, 
-          early_stopping=True,
-          num_return_sequences=3)
-
-      corrected = []
-      for pred in preds:  
-        corrected.append(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
-
-      scores = self.scorer.sentence_score(corrected, log=True)
-      ranked_corrected = [(c,s) for i, s in zip(corrected, scores)]
-      ranked_corrected.sort(key = lambda x:x[1], reverse=True)
-
-      orig = input_sentence[5:].strip()
-      cor  = ranked_corrected[0][0]   
-      edits = _get_edits(orig, cor)
-
-      return edits
-
-   
-
   def detect(self, input_sentence):
-      # TO BE IMPLEMENTED
-      pass
+        # TO BE IMPLEMENTED
+        pass
+
+  def _get_edits(self, orig, cor):
+        orig = self.annotator.parse(orig)
+        cor = self.annotator.parse(cor)
+        alignment = self.annotator.align(orig, cor)
+        edits = self.annotator.merge(alignment)
+
+        if len(edits) == 0:  
+            return []
+
+        edit_annotations = []
+        for e in edits:
+            e = self.annotator.classify(e)
+            edit_annotations.append((e.type[2:], e.o_str, e.o_start, e.o_end,  e.c_str, e.c_start, e.c_end))
+                
+        if len(edit_annotations) > 0:
+            return edit_annotations
+        else:    
+            return []
+
+  def get_edits(self, input_sentence):
+        correction_prefix = "gec: "
+        input_sentence = correction_prefix + input_sentence
+        input_ids = self.correction_tokenizer.encode(input_sentence, return_tensors='pt')
+        input_ids = input_ids.to(self.device)
+
+        preds = self.correction_model.generate(
+            input_ids,
+            do_sample=True, 
+            max_length=128, 
+            top_k=50, 
+            top_p=0.95, 
+            early_stopping=True,
+            num_return_sequences=3)
+
+        corrected = []
+        for pred in preds:  
+            corrected.append(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
+
+        scores = self.scorer.sentence_score(corrected, log=True)
+        ranked_corrected = [(c,s) for i, s in zip(corrected, scores)]
+        ranked_corrected.sort(key = lambda x:x[1], reverse=True)
+
+        orig = input_sentence[5:].strip()
+        cor  = ranked_corrected[0][0]   
+        edits = _get_edits(orig, cor)
+
+        return edits
+        
+   
