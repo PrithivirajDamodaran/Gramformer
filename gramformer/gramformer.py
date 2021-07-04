8931865f76edc949df2e90081a61b9b5be4f365d
Prithivida <d.prithiviraj@gmail.com>
2021-07-04 11:06:43 +0530

    changes for QE estimator

--- gramformer/gramformer.py
+++ gramformer/gramformer.py
@@ -3,14 +3,16 @@ class Gramformer:
   def __init__(self, models=2, use_gpu=False):
     from transformers import AutoTokenizer
     from transformers import AutoModelForSeq2SeqLM
+    from lm_scorer.models.auto import AutoLMScorer as LMScorer
     import errant
     self.annotator = errant.load('en')
-
+    
     if use_gpu:
         device= "cuda:0"
     else:
         device = "cpu"
-
+    batch_size = 1    
+    self.scorer = LMScorer.from_pretrained("gpt2", device=device, batch_size=batch_size)    
     self.device    = device
     correction_model_tag = "prithivida/grammar_error_correcter_v1"
     
@@ -18,7 +20,7 @@ class Gramformer:
         self.correction_tokenizer = AutoTokenizer.from_pretrained(correction_model_tag)
         self.correction_model     = AutoModelForSeq2SeqLM.from_pretrained(correction_model_tag)
         self.correction_model     = self.correction_model.to(device)
-        print("[Gramformer] Grammar error correction model loaded..")
+        print("[Gramformer] Grammar error correct/highlight model loaded..")
     elif models == 3:
         self.correction_tokenizer = AutoTokenizer.from_pretrained(correction_model_tag)
         self.correction_model     = AutoModelForSeq2SeqLM.from_pretrained(correction_model_tag)
@@ -26,7 +28,7 @@ class Gramformer:
         print("[Gramformer] All models loaded..")    
     
 
-  def correct(self, input_sentence, max_corrections=1):
+  def correct(self, input_sentence, max_candidates=3):
       correction_prefix = "gec: "
       input_sentence = correction_prefix + input_sentence
       input_ids = self.correction_tokenizer.encode(input_sentence, return_tensors='pt')
@@ -39,13 +41,16 @@ class Gramformer:
           top_k=50, 
           top_p=0.95, 
           early_stopping=True,
-          num_return_sequences=max_corrections)
+          num_return_sequences=max_candidates)
 
       corrected = []
       for pred in preds:  
         corrected.append(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
 
-      return corrected
+      scores = self.scorer.sentence_score(corrected, log=True)
+      ranked_corrected = [(c,s) for i, s in zip(corrected, scores)]
+      ranked_corrected.sort(key = lambda x:x[1], reverse=True)
+      return ranked_corrected[0][0]
 
   def highlight(self, input_sentence):
      # TO BE IMPLEMENTED
@@ -84,14 +89,18 @@ def get_edits(self, input_sentence):
           top_k=50, 
           top_p=0.95, 
           early_stopping=True,
-          num_return_sequences=1)
+          num_return_sequences=3)
 
       corrected = []
       for pred in preds:  
         corrected.append(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
 
+      scores = self.scorer.sentence_score(corrected, log=True)
+      ranked_corrected = [(c,s) for i, s in zip(corrected, scores)]
+      ranked_corrected.sort(key = lambda x:x[1], reverse=True)
+
       orig = input_sentence[5:].strip()
-      cor  = corrected[0]   
+      cor  = ranked_corrected[0][0]   
       edits = _get_edits(orig, cor)
 
       return edits
