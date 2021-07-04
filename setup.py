d9f1005db522402b58cd1d3917c7101e152fb41c
Prithivida <d.prithiviraj@gmail.com>
2021-07-04 11:11:21 +0530

    changes for QE estimator

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="A framework for detecting, highlighting and correcting grammatical errors on natural language text",
     url="https://github.com/PrithivirajDamodaran/Gramformer.git",
     packages=setuptools.find_packages(),
-    install_requires=['transformers==4.6.1', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'fsspec==2021.5.0', 'lm-scorer==0.4.2'],
+    install_requires=['transformers', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'fsspec==2021.5.0', 'lm-scorer==0.4.2'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: Apache 2.0",
