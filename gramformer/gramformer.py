bcc94375cd4dfdf1c37c3fbda872ed1b55c2ef69
Prabhkaran Singh <39077923+prabhkaran@users.noreply.github.com>
2021-07-08 12:32:42 +0530

    PR for highlight
    Added highlighter by reusing get_edits. 
    Highlight and get_edits can be nested with the output of correct. 
    Changed model numbers now 1 is the corrector.

--- gramformer/gramformer.py
+++ gramformer/gramformer.py
@@ -1,6 +1,6 @@
 class Gramformer:
 
-  def __init__(self, models=2, use_gpu=False):
+  def __init__(self, models=1, use_gpu=False):
     from transformers import AutoTokenizer
     from transformers import AutoModelForSeq2SeqLM
     from lm_scorer.models.auto import AutoLMScorer as LMScorer
@@ -15,46 +15,90 @@ class Gramformer:
     self.scorer = LMScorer.from_pretrained("gpt2", device=device, batch_size=batch_size)    
     self.device    = device
     correction_model_tag = "prithivida/grammar_error_correcter_v1"
-    
-    if models == 1 or models == 2:
+    self.model_loaded = False
+
+    if models == 1:
         self.correction_tokenizer = AutoTokenizer.from_pretrained(correction_model_tag)
         self.correction_model     = AutoModelForSeq2SeqLM.from_pretrained(correction_model_tag)
         self.correction_model     = self.correction_model.to(device)
+        self.model_loaded = True
         print("[Gramformer] Grammar error correct/highlight model loaded..")
-    elif models == 3:
-        self.correction_tokenizer = AutoTokenizer.from_pretrained(correction_model_tag)
-        self.correction_model     = AutoModelForSeq2SeqLM.from_pretrained(correction_model_tag)
-        self.correction_model     = self.correction_model.to(device)
-        print("[Gramformer] All models loaded..")    
-    
+    elif models == 2:
+        # TODO
+        print("TO BE IMPLEMENTED!!!")
 
   def correct(self, input_sentence, max_candidates=1):
-      correction_prefix = "gec: "
-      input_sentence = correction_prefix + input_sentence
-      input_ids = self.correction_tokenizer.encode(input_sentence, return_tensors='pt')
-      input_ids = input_ids.to(self.device)
+      if self.model_loaded:
+        correction_prefix = "gec: "
+        input_sentence = correction_prefix + input_sentence
+        input_ids = self.correction_tokenizer.encode(input_sentence, return_tensors='pt')
+        input_ids = input_ids.to(self.device)
 
-      preds = self.correction_model.generate(
-          input_ids,
-          do_sample=True, 
-          max_length=128, 
-          top_k=50, 
-          top_p=0.95, 
-          early_stopping=True,
-          num_return_sequences=max_candidates)
+        preds = self.correction_model.generate(
+            input_ids,
+            do_sample=True, 
+            max_length=128, 
+            top_k=50, 
+            top_p=0.95, 
+            early_stopping=True,
+            num_return_sequences=max_candidates)
 
-      corrected = []
-      for pred in preds:  
-        corrected.append(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
+        corrected = set()
+        for pred in preds:  
+          corrected.add(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
 
-      scores = self.scorer.sentence_score(corrected, log=True)
-      ranked_corrected = [(c,s) for c, s in zip(corrected, scores)]
-      ranked_corrected.sort(key = lambda x:x[1], reverse=True)
-      return ranked_corrected
+        corrected = list(corrected)
+        scores = self.scorer.sentence_score(corrected, log=True)
+        ranked_corrected = [(c,s) for c, s in zip(corrected, scores)]
+        ranked_corrected.sort(key = lambda x:x[1], reverse=True)
+        return ranked_corrected
+      else:
+        print("Model is not loaded")  
+        return None
 
-  def highlight(self, input_sentence):
-     # TO BE IMPLEMENTED
-     pass
+  def highlight(self, orig, cor):
+      edits = self._get_edits(orig, cor)
+      orig_tokens = orig.split()
+
+      ignore_indexes = []
+
+      for edit in edits:
+          edit_type = edit[0]
+          edit_str_start = edit[1]
+          edit_spos = edit[2]
+          edit_epos = edit[3]
+          edit_str_end = edit[4]
+
+          # if no_of_tokens(edit_str_start) > 1 ==> excluding the first token, mark all other tokens for deletion
+          for i in range(edit_spos+1, edit_epos):
+            ignore_indexes.append(i)
+
+          if edit_str_start == "":
+              if edit_spos - 1 >= 0:
+                  new_edit_str = orig_tokens[edit_spos - 1]
+                  edit_spos -= 1
+              else:
+                  new_edit_str = orig_tokens[edit_spos + 1]
+                  edit_spos += 1
+              if edit_type == "PUNCT":
+                st = "<a type='" + edit_type + "' edit='" + \
+                    edit_str_end + "'>" + new_edit_str + "</a>"
+              else:
+                st = "<a type='" + edit_type + "' edit='" + new_edit_str + \
+                    " " + edit_str_end + "'>" + new_edit_str + "</a>"
+              orig_tokens[edit_spos] = st
+          elif edit_str_end == "":
+            st = "<d type='" + edit_type + "' edit=''>" + edit_str_start + "</d>"
+            orig_tokens[edit_spos] = st
+          else:
+            st = "<c type='" + edit_type + "' edit='" + \
+                edit_str_end + "'>" + edit_str_start + "</c>"
+            orig_tokens[edit_spos] = st
+
+      for i in ignore_indexes:
+        del(orig_tokens[i])
+
+      return(" ".join(orig_tokens))
 
   def detect(self, input_sentence):
         # TO BE IMPLEMENTED
@@ -79,33 +123,5 @@ class Gramformer:
         else:    
             return []
 
-  def get_edits(self, input_sentence):
-        correction_prefix = "gec: "
-        input_sentence = correction_prefix + input_sentence
-        input_ids = self.correction_tokenizer.encode(input_sentence, return_tensors='pt')
-        input_ids = input_ids.to(self.device)
-
-        preds = self.correction_model.generate(
-            input_ids,
-            do_sample=True, 
-            max_length=128, 
-            top_k=50, 
-            top_p=0.95, 
-            early_stopping=True,
-            num_return_sequences=3)
-
-        corrected = []
-        for pred in preds:  
-            corrected.append(self.correction_tokenizer.decode(pred, skip_special_tokens=True).strip())
-
-        scores = self.scorer.sentence_score(corrected, log=True)
-        ranked_corrected = [(c,s) for c, s in zip(corrected, scores)]
-        ranked_corrected.sort(key = lambda x:x[1], reverse=True)
-
-        orig = input_sentence[5:].strip()
-        cor  = ranked_corrected[0][0]   
-        edits = self._get_edits(orig, cor)
-
-        return edits
-        
-   
+  def get_edits(self, orig, cor):
+      return self._get_edits(orig, cor)
